1991ad8e073e2c3e4f454de15e9999227f63fafd
Ainagul Shabdanova <shabdanova.ainagul.kk@gmail.com>
2023-12-06 21:44:48 +0600

    feat: create html structure of blog

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -1,9 +1,39 @@
 import React from 'react'
+import Link from 'next/link'
+import Image from 'next/image'
+
 import styles from './page.module.css'
 
 const Blog = () => {
   return (
-    <div>Blog</div>
+    <div className={styles.mainContainer}>
+      <Link href='blog/test' className={styles.container} key='1'>
+        <div className={styles.imageContainer}>
+          <Image src=""
+              alt=""
+              width={400}
+              height={250}
+              className={styles.image}/>
+        </div>
+        <div className={styles.content}>
+          <h1 className={styles.title}>Title</h1>
+          <p className={styles.desc}>desc</p>
+        </div>
+      </Link>
+      <Link href='blog/test2' className={styles.container} key='2'>
+        <div className={styles.imageContainer}>
+          <Image src=""
+              alt=""
+              width={400}
+              height={250}
+              className={styles.image}/>
+        </div>
+        <div className={styles.content}>
+          <h1 className={styles.title}>Title</h1>
+          <p className={styles.desc}>desc</p>
+        </div>
+      </Link>
+    </div>
   )
 }
 
