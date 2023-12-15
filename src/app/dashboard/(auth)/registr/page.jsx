9bbff4e7794a05f6301ba24cd62183eae4a77e5b
Ainagul Shabdanova <shabdanova.ainagul.kk@gmail.com>
2023-12-15 21:44:56 +0600

    feat: create user's registartion feature

--- src/app/dashboard/(auth)/registr/page.jsx
+++ src/app/dashboard/(auth)/registr/page.jsx
@@ -1,8 +1,69 @@
-import React from 'react'
+"use client"
+
+import { useState } from 'react'
+import styles from './page.module.css'
+import Link from 'next/link'
+import { useRouter } from 'next/navigation'
 
 const Registr = () => {
+  const [error, setError] = useState(null)
+  const router = useRouter()
+
+  const handleSubmit = async(e) => {
+    e.preventDefault()
+    const userName = e.target[0].value
+    const email = e.target[1].value
+    const password = e.target[2].value
+
+    try {
+      const res = await fetch("/api/auth/register", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          userName,
+          email,
+          password
+        }),
+      });
+      res.status === 201 && router.push("/dashboard/login?success= Account has been created")
+    } catch (error) {
+      setError(error)
+      console.log(error);
+    }
+  }
+
+
   return (
-    <div>Registr</div>
+    <div className={styles.container}>
+      <h1 className={styles.title}>Create an Account</h1>
+      <h2 className={styles.subtitle}>Please sign up to see dashboard</h2>
+      <form className={styles.form} onSubmit={handleSubmit}>
+        <input 
+          type="text" 
+          placeholder='Username' 
+          required 
+          className={styles.input}
+        />
+        <input 
+          type="email" 
+          placeholder='Email' 
+          required 
+          className={styles.input}
+        />
+        <input 
+          type="password" 
+          placeholder='Password' 
+          required 
+          className={styles.input}
+        />
+        <button className={styles.button}>Register</button>
+        {error && "Something went wrong!"}
+      </form>
+      <span className={styles.or}>- OR -</span>
+      <Link className={styles.link} href="/dashboard/login">Login with an existing Account</Link>
+    </div>
   )
 }
 
